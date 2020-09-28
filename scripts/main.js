acd96624e35a31b1f8d6a4bdf9a9b2a66ad0068b
Dickson <nyangedickson@gmail.com>
2020-09-29 00:55:20 +0300

    adding modal and delete functionality

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,17 @@ let newNote = document.getElementById('newNote');
 let closeModal = document.getElementById('closeModal');
 const modal = document.querySelector('.modal-container');
 
+let content = document.querySelector('.card-container');
+
 newNote.addEventListener('click', getModal);
 closeModal.addEventListener('click', closeModalNow);
+content.addEventListener('click', (e) => {
+    if(e.target.classList[1] === 'delete') {
+        let childEl = e.target.parentElement.parentElement;
+        content.removeChild(childEl);
+    }
+});
+
 
 //get modal
 function getModal() {
